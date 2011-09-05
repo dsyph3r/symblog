635875cdfad44b6f34e22c9ba833df877846b6a3
d.syph.3r <d.syph.3r@googlemail.com>
2011-09-05 20:49:17 +0100

    adding Twig extension for created_ago filter

---
+++ src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Blogger\BlogBundle\Twig\Extensions;
+
+class BloggerBlogExtension extends \Twig_Extension
+{
+
+    public function getFilters()
+    {
+        return array(
+            'created_ago' => new \Twig_Filter_Method($this, 'createdAgo'),
+        );
+    }
+
+    public function createdAgo(\DateTime $dateTime)
+    {
+        $delta = time() - $dateTime->getTimestamp();
+        if ($delta < 0)
+            throw new Exception("createdAgo is unable to handle dates in the future");
+
+        $duration = "";
+        if ($delta < 60)
+        {
+          // Seconds
+          $time = $delta;
+          $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else if ($delta <= 3600)
+        {
+          // Mins
+          $time = floor($delta / 60);
+          $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else if ($delta <= 86400)
+        {
+          // Hours
+          $time = floor($delta / 3600);
+          $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else
+        {
+          // Days
+          $time = floor($delta / 86400);
+          $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
+        }
+
+        return $duration;
+    }
+
+    public function getName()
+    {
+        return 'blogger_blog_extension';
+    }
+}
