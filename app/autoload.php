7f617ed9e605e783ff409a800e72e148465aac36
d.syph.3r <d.syph.3r@googlemail.com>
2011-09-05 19:10:55 +0100

    removed Acme demo bundle

--- app/autoload.php
+++ app/autoload.php
@@ -5,17 +5,17 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
+    'Symfony'                           => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Sensio'                            => __DIR__.'/../vendor/bundles',
+    'JMS'                               => __DIR__.'/../vendor/bundles',
     'Doctrine\\Common\\DataFixtures'    => __DIR__.'/../vendor/doctrine-fixtures/lib',
     'Doctrine\\Common'                  => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL\\Migrations' => __DIR__.'/../vendor/doctrine-migrations/lib',
-    'Doctrine\\DBAL'             => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    'Doctrine\\DBAL\\Migrations'        => __DIR__.'/../vendor/doctrine-migrations/lib',
+    'Doctrine\\DBAL'                    => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'                          => __DIR__.'/../vendor/doctrine/lib',
+    'Monolog'                           => __DIR__.'/../vendor/monolog/src',
+    'Assetic'                           => __DIR__.'/../vendor/assetic/src',
+    'Metadata'                          => __DIR__.'/../vendor/metadata/src',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
