f240385fd4501e1f75572818dd43bc13fa32fddd
dsyph3r <d.syph.3r@googlemail.com>
2011-09-11 10:20:46 +0200

    formatting correction

--- src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
+++ src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
@@ -21,27 +21,27 @@ class BloggerBlogExtension extends \Twig_Extension
         $duration = "";
         if ($delta < 60)
         {
-          // Seconds
-          $time = $delta;
-          $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
+            // Seconds
+            $time = $delta;
+            $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
         }
         else if ($delta <= 3600)
         {
-          // Mins
-          $time = floor($delta / 60);
-          $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
+            // Mins
+            $time = floor($delta / 60);
+            $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
         }
         else if ($delta <= 86400)
         {
-          // Hours
-          $time = floor($delta / 3600);
-          $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
+            // Hours
+            $time = floor($delta / 3600);
+            $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
         }
         else
         {
-          // Days
-          $time = floor($delta / 86400);
-          $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
+            // Days
+            $time = floor($delta / 86400);
+            $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
         }
 
         return $duration;
