4b80426fd97b1d1cde179aaa94485302b6081118
d.syph.3r <d.syph.3r@googlemail.com>
2011-09-13 20:33:41 +0100

    adding tests for contact page

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -33,4 +33,45 @@ class PageControllerTest extends WebTestCase
         $this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle .'")')->count());
     }
     
+    public function testContact()
+    {
+        $client = static::createClient();
+    
+        $crawler = $client->request('GET', '/contact');
+    
+        $this->assertEquals(1, $crawler->filter('h1:contains("Contact symblog")')->count());
+    
+        // Select based on button value, or id or name for buttons
+        $form = $crawler->selectButton('Submit')->form();
+    
+        $form['blogger_blogbundle_enquirytype[name]']       = 'name';
+        $form['blogger_blogbundle_enquirytype[email]']      = 'email@email.com';
+        $form['blogger_blogbundle_enquirytype[subject]']    = 'Subject';
+        $form['blogger_blogbundle_enquirytype[body]']       = 'The comment body must be at least 50 characters long as there is a validation constrain on the Enquiry entity';
+    
+        $crawler = $client->submit($form);
+
+        // Check email has been sent
+        if ($profile = $client->getProfile())
+        {
+            $swiftMailerProfiler = $profile->getCollector('swiftmailer');
+
+            // Only 1 message should have been sent
+            $this->assertEquals(1, $swiftMailerProfiler->getMessageCount());
+
+            // Get the first message
+            $messages = $swiftMailerProfiler->getMessages();
+            $message  = array_shift($messages);
+
+            $symblogEmail = $client->getContainer()->getParameter('blogger_blog.emails.contact_email');
+            // Check message is being sent to correct address
+            $this->assertArrayHasKey($symblogEmail, $message->getTo());
+        }
+
+        // Need to follow redirect
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('.blogger-notice:contains("Your contact enquiry was successfully sent. Thank you!")')->count() > 0);
+    }
+    
 }
