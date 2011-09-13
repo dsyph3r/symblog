1762c2cc71b613a65d7eeda6a906506e415dcdef
d.syph.3r <d.syph.3r@googlemail.com>
2011-09-13 20:22:01 +0100

    adding functional test for homepage

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -14,4 +14,23 @@ class PageControllerTest extends WebTestCase
 
         $this->assertEquals(1, $crawler->filter('h1:contains("About symblog")')->count());
     }
+    
+    public function testIndex()
+    {
+        $client = static::createClient();
+    
+        $crawler = $client->request('GET', '/');
+    
+        // Check there are some blog entries on the page
+        $this->assertTrue($crawler->filter('article.blog')->count() > 0);
+        
+        // Find the first link, get the title, ensure this is loaded on the next page
+        $blogLink   = $crawler->filter('article.blog h2 a')->first();
+        $blogTitle  = $blogLink->text();
+        $crawler    = $client->click($blogLink->link());
+
+        // Check the h2 has the blog title in it
+        $this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle .'")')->count());
+    }
+    
 }
